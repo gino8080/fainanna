2164c04428f0320f8d7e943a414dd232f9ac6b5b
Magico <magie.cannizzaro@gmail.com>
2021-06-07 10:27:50 +0200

    BTLTE

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,12 +3,24 @@ import {
   BleClient,
   numberToUUID,
   ScanResult,
+  numbersToDataView,
 } from '@capacitor-community/bluetooth-le';
 import { sortby } from 'lodash';
-import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 
 const UUID_SERVICE = '00001101-0000-1000-8000-00805F9B34FB'.toLowerCase();
+const UUID_FLN = '0000ffe0-0000-1000-8000-00805f9b34fb';
 const FLN_DEVICE_HMAC = '';
+
+const HEART_RATE_SERVICE = '0000180d-0000-1000-8000-00805f9b34fb';
+const HEART_RATE_MEASUREMENT_CHARACTERISTIC =
+  '00002a37-0000-1000-8000-00805f9b34fb';
+const BODY_SENSOR_LOCATION_CHARACTERISTIC =
+  '00002a38-0000-1000-8000-00805f9b34fb';
+const BATTERY_SERVICE = numberToUUID(0x180f);
+const BATTERY_CHARACTERISTIC = numberToUUID(0x2a19);
+const POLAR_PMD_SERVICE = 'fb005c80-02e7-f387-1cad-8acd2d8df0c8';
+const POLAR_PMD_CONTROL_POINT = 'fb005c81-02e7-f387-1cad-8acd2d8df0c8';
+
 interface Device extends ScanResult {
   data: any;
 }
@@ -22,32 +34,11 @@ export class Tab1Page {
 
   public logs: string[] = [];
 
-  constructor(private bluetoothSerial: BluetoothSerial) { }
+  public device: Device;
+  constructor() {}
 
   get deviceLogs() {
     return this.logs;
-  }
-  async connect() {
-    this.logs = [];
-    this.bluetoothSerial.connect(FLN_DEVICE_HMAC).subscribe(
-      (connected) => {
-        console.log('🚀 ~ this.bluetoothSerial.connect ~ connected', connected);
-        this.logs.push('connected to ' + connected);
-        //this.checkData();
-        this.bluetoothSerial.subscribe('\n').subscribe(
-          (data) => {
-            console.log('🚀 ~ data', data);
-            this.logs.push(data);
-          },
-          (err) => {
-            console.error(err);
-          }
-        );
-      },
-      (err) => {
-        console.error(err);
-      }
-    );
   }
 
   // checkData() {
@@ -60,13 +51,7 @@ export class Tab1Page {
 
   vibration(enable) {
     const data = enable ? 'FLN_CMD {VON}' : 'FLN_CMD {VOFF}';
-    this.bluetoothSerial
-      .write(data)
-      .then((success) => {
-        console.log('🚀 ~ this.bluetoothSerial.write ~ ', data);
-        this.logs.push(data);
-      })
-      .catch((err) => console.error(err));
+    console.log('🚀 ~ vibration ~ data', data);
   }
 
   async requestLEScan(): Promise<void> {
@@ -100,20 +85,98 @@ export class Tab1Page {
     try {
       await BleClient.initialize();
       const device = await BleClient.requestDevice({
+        services: [UUID_FLN],
+        namePrefix: 'SH-HC',
+
         //services: [UUID_SERVICE],
-        namePrefix: 'HC',
+        // optionalServices: [BATTERY_SERVICE, POLAR_PMD_SERVICE],
+
+        //name:
       });
       alert(JSON.stringify(device, null, 2));
+
       await BleClient.connect(device.deviceId).then((connected) => {
         alert('connected to ' + device.name);
       });
 
       //await BleClient.read(device.deviceId)
+
+      // const result = await BleClient.read(
+      //   device.deviceId,
+      //   HEART_RATE_SERVICE,
+      //   BODY_SENSOR_LOCATION_CHARACTERISTIC
+      // );
+      // console.log('body sensor location', result.getUint8(0));
+
+      // const battery = await BleClient.read(
+      //   device.deviceId,
+      //   BATTERY_SERVICE,
+      //   BATTERY_CHARACTERISTIC
+      // );
+      // console.log('battery level', battery.getUint8(0));
+
+      // await BleClient.write(
+      //   device.deviceId,
+      //   POLAR_PMD_SERVICE,
+      //   POLAR_PMD_CONTROL_POINT,
+      //   numbersToDataView([1, 0])
+      // );
+      // console.log('written [1, 0] to control point');
+
+      await BleClient.startNotifications(
+        device.deviceId,
+        UUID_FLN,
+        UUID_FLN,
+        (value) => {
+          console.log('current heart rate', value);
+        }
+      );
+
+      // setTimeout(async () => {
+      //   await BleClient.stopNotifications(
+      //     device.deviceId,
+      //     HEART_RATE_SERVICE,
+      //     HEART_RATE_MEASUREMENT_CHARACTERISTIC
+      //   );
+      //   await BleClient.disconnect(device.deviceId);
+      //   console.log('disconnected from device', device);
+      // }, 10000);
     } catch (error) {
       console.error(error);
     }
   }
 
+  async connect(device: Device): Promise<void> {
+    console.log('connect DEVICE', device);
+    debugger;
+    const deviceId = device.device.deviceId;
+    try {
+      await BleClient.disconnect(deviceId);
+      await BleClient.connect(deviceId);
+      console.log('connected to device', device);
+      this.device = device;
+      // const result = await BleClient.read(
+      //   device.deviceId,
+      //   HEART_RATE_SERVICE,
+      //   BODY_SENSOR_LOCATION_CHARACTERISTIC,
+      // );
+      // console.log('body sensor location', result.getUint8(0));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async disconnect(): Promise<void> {
+    console.log('connect', this.device);
+
+    try {
+      await BleClient.disconnect(this.device.device.deviceId);
+
+      console.log('disconnected to device', this.device);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   get deviceList() {
     const devices = this.devices.map((device) => {
       device.data = JSON.stringify(device.manufacturerData);
@@ -121,6 +184,6 @@ export class Tab1Page {
     });
     //console.log('🚀 ~ devices ~ devices', devices);
     return devices;
-    return sortby(devices, ['rssi']);
+    //return sortby(devices, ['rssi']);
   }
 }
